f386920f5ed419d7af2ae2d02736775931d2503d
gavinliu266 <772219189@qq.com>
2023-07-22 21:04:43 +0800

    修复及优化
    修复及优化

--- init.el
+++ init.el
@@ -50,12 +50,13 @@
        (when (not (package-installed-p pkg)) (setq tmp nil)))
   tmp)
 
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-	 (package-install pkg))))
+(unless (my/packages-installed-p)
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg my/packages)
+      (when (not (package-installed-p pkg))
+	(package-install pkg))))
+(use-package all-the-icons)
 
 ;========================================================
 ;basic Settings
@@ -75,14 +76,13 @@
 (setq system-time-locale "C")
 (setq auto-save-default nil
       make-backup-files nil)
-(use-package emacs :config (defalias 'yes-or-no-p 'y-or-n-p))
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq gc-cons-threshold (* 50 1000 1000))
 ;;==============================================
 ;;hightlight-symbol
 ;;=============================================
 (use-package highlight-symbol
-  :defer 1
   :init (highlight-symbol-mode)
   :bind ("<f3>" . highlight-symbol))
 
@@ -94,7 +94,6 @@
 ;================================================
 
 (use-package dashboard
-  :defer 1
   :init
   (setq dashboard-banner-logo-title "Welcome to Emacs!")
   (setq-default dashboard-startup-banner 'logo)
@@ -113,30 +112,25 @@
 ;;============================================
 ;;which-key
 ;;===============================================
-(use-package which-key
-  :defer 1
-  :init (which-key-mode))
+(which-key-mode t)
 
 
 ;;===============================================
 ;;neotree
 ;;================================================
 (use-package neotree
-  :defer 1
-  :config
+  :init
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   (setq-default neo-show-hidden-files t)
   (setq neo-window-fixed-size nil)
   (setq-default neo-window-width 37)
   (neotree))
 
-
 ;;=======================================
 ;;orgSet
 ;;========================================
 ;;  (require 'org-bullets)
 (use-package org-bullets
-  :defer 1
   :init
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
   (setq org-todo-keywords
@@ -150,12 +144,9 @@
 ;=================================================
 ;corfu
 ;;================================================
-(use-package corfu
-  :defer 1
-  :init
-  (global-corfu-mode)
-  (setq corfu-auto t
-	corfu-quit-no-match 'separator))
+(global-corfu-mode)
+(setq corfu-auto t
+      corfu-quit-no-match 'separator)
 
 (use-package kind-icon ;;kind-icon美化
   :after corfu
@@ -165,67 +156,45 @@
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 (use-package cape
-  :defer 1
   :init
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-elisp-block))
-
 ;;===========================================================
 ;;orderless
 ;;===========================================================
-(use-package orderless
-  :defer 1
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles . (partial-completion))))))
+(setq completion-styles '(orderless basic)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles . (partial-completion)))))
 
 ;;=======================================
 ;;vertico
 ;;======================================
 ;; Enable vertico
-(use-package vertico
-  :defer 1
-  :init
-  (vertico-mode))
-(use-package savehist
-  :defer 1
-  :init
-  (savehist-mode))
-(use-package emacs
-  :defer 1
-  :init
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-  (setq enable-recursive-minibuffers t))
+(vertico-mode)
+(savehist-mode)
+(defun crm-indicator (args)
+  (cons (format "[CRM%s] %s"
+                (replace-regexp-in-string
+                 "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                 crm-separator)
+                (car args))
+        (cdr args)))
+(advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+(setq minibuffer-prompt-properties
+      '(read-only t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+(setq enable-recursive-minibuffers t)
 
 ;================================================
 ;racket
 ;;===============================================
-(use-package racket-mode
-  :defer 1
-  :init
-  (setq scheme-program-name "racket"))
+(setq scheme-program-name "racket")
 
 ;;================================================
 ;;undo-tree
 ;;================================================
-(use-package undo-tree
-  :defer 1
-  :init (global-undo-tree-mode t))
+(global-undo-tree-mode t)
 
 
 ;;=================================================
@@ -244,66 +213,36 @@
 ;;rainbow-delimiters
 ;;=============================================
 (use-package rainbow-delimiters
-  :defer 1
   :hook (prog-mode . rainbow-delimiters-mode))
 
 ;;==============================================
 ;;flycheck
 ;;==============================================
-(use-package flycheck
-  :defer 1
-  :init
-  (global-flycheck-mode 1))
+(global-flycheck-mode 1)
 
 ;=================================================
 ;goodScroll
 ;;=================================================
-(use-package good-scroll
-  :defer 1
-  :init
-  (good-scroll-mode t))
+(good-scroll-mode t)
 
 ;==================================================
 ;有道翻译
 ;==================================================
 ;;youdao
-(use-package youdao-dictionary
-  :defer 1
-  :init
 (setq url-automatic-caching t)
 (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
-(setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao"))
+(setq youdao-dictionary-search-history-file "~/.emacs.d/.youdao")
 
 ;;swiper
 (use-package swiper
-  :defer 1
   :init
 (global-set-key (kbd "C-s") 'swiper))
-
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(monokai))
- '(custom-safe-themes
-   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" "288482f5c627c1fe5a1d26fcc17ec6ca8837f36bf940db809895bf3f8e2e4edd" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
- '(display-time-mode t)
- '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   '(company cape vertico orderless neotree kind-icon corfu hydra yasnippet-snippets highlight-symbol use-package-hydra which-key spaceline atom-one-dark-theme undo-tree rainbow-delimiters yasnippets auctex dashboard swiper youdao-dictionary monokai-theme))
- '(size-indication-mode t)
- '(tool-bar-mode nil))
 
 
 ;;======================================================
 ;;spaceline
 ;;=====================================================;
 (use-package spaceline
-  :defer 1
   :config
   (use-package spaceline-config
     :config
@@ -316,3 +255,23 @@
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(monokai))
+ '(custom-safe-themes
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "0c860c4fe9df8cff6484c54d2ae263f19d935e4ff57019999edbda9c7eda50b8" "288482f5c627c1fe5a1d26fcc17ec6ca8837f36bf940db809895bf3f8e2e4edd" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
+ '(display-time-mode t)
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages
+   '(all-the-icons good-scroll racket-mode org-bullets yasnippet flycheck treemacs-all-the-icons company cape vertico orderless neotree kind-icon corfu hydra yasnippet-snippets highlight-symbol use-package-hydra which-key spaceline atom-one-dark-theme undo-tree rainbow-delimiters yasnippets auctex dashboard swiper youdao-dictionary monokai-theme))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Cascadia Code" :foundry "outline" :slant normal :weight normal :height 102 :width normal)))))
